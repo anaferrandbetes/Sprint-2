3ef421b3699349c2c08e59a371f062af4f7da8f6
anaferrandbetes <anaferrandbetes@gmail.com>
2025-06-25 12:05:42 +0200

    Delete Nivell_2_Exercici_2.sql

--- Nivell_2_Exercici_2.sql
+++
@@ -1,6 +0,0 @@
--- Exercici 2: Quina és la mitjana de vendes per país? Presenta els resultats ordenats de major a menor mitjà.
-SELECT company.country, company.id, company.company_name, AVG(transaction.amount) AS avg_amount
-FROM company
-INNER JOIN transaction ON company.id = transaction.company_id
-GROUP BY company.country, company.id, company.company_name
-ORDER BY avg_amount DESC;
